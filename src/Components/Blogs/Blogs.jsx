02792be4ea31c63b3162b5a5bdeaa253d618f02a
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-02 18:16:54 +0600

    Blogs section components create and import to App.jsx

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Blogs = () => {
+    const [] = useState([]);
+    return (
+        <>
+            <h2>Blogs:</h2>
+        </>
+    );
+};
+
+export default Blogs;
